a9d2414c0f24354e1183d2503cf79e989f56330e
NickLawrence0025 <101275002+NickLawrence0025@users.noreply.github.com>
2022-03-16 07:14:11 +0000

    Update Wk1 Task1 - Guessing Game.py

--- Wk1 Task1 - Guessing Game.py
+++ Wk1 Task1 - Guessing Game.py
@@ -2,14 +2,14 @@ import random
 from time import sleep
 
 my_name= input("Hello! What is your name?")
+my_name = my_name.capitalize()
 number = random.randint(1, 10)
-print("Well, " + my_name +"I am thinking of a number between 1 and 10.")
+print("Well, " + my_name +" I am thinking of a number between 1 and 10.")
 guess = int(input("Take a guess:"))
 print("Were you correct?...")
-sleep(3)
+sleep(1)
 if guess == number:
     print("Good job,", my_name + "! You guessed my number")
 else:
-    print("Wrong, better luck next time. I was thinking of the number ", + number)
-
+    print("Wrong, better luck next time. I was thinking of the number", + number)
 
